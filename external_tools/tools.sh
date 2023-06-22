f42b25939cc1216f7cd112404451939dac6de848
Andre Korporaal <andrekorporaal@gmail.com>
2023-06-22 20:36:20 +0200

    Add laser prep to the tools.sh file

--- external_tools/tools.sh
+++ external_tools/tools.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+source ../env/bin/activate
+
+function laser_prep {
+    export LASER=`realpath "./LASER"`
+    cd "./LASER"
+        bash ./nllb/download_models.sh ace_Latn
+        bash install_external_tools.sh
+        sed -i 's/model_dir=""/model_dir=$LASER/g' ./tasks/embed/embed.sh
+        sed -i 's/over_write=False/over_write=True/g' ./source/embed.py
+    cd ..
+}
+
+
 #####################################
 ### Cross-lingual static embeddings
 #####################################
@@ -14,6 +27,7 @@ git clone https://github.com/artetxem/vecmap.git
 
 # LASER - A library to calculate and use multilingual sentence embeddings
 git clone https://github.com/facebookresearch/LASER.git
+laser_prep
 
 ##############################
 ### Word alignment tools
